da01a016d9260f3a83e170f11d7411196edbfe8c
Kaio <prandini.kaio@gmail.com>
2023-10-24 23:54:53 -0300

    Vinculado cartao de credito da conta, que vai controlar os lancamentos de saída

---
+++ src/main/java/com/prandini/personal/conta/domain/CreditCard.java
@@ -0,0 +1,28 @@
+package com.prandini.personal.conta.domain;
+
+import com.prandini.personal.lancamento.domain.Lancamento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity(name = "credito")
+@Table(name = "creditos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Credito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Conta conta;
+    private BigDecimal limite;
+    private BigDecimal limiteUtilizado;
+    private int diaVencimento;
+    @OneToMany
+    private List<Lancamento> lancamentos;
+}
